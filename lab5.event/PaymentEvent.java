ab5e81948d138c229221e4a2b9027c3fff7da537
maxforsgren <61727544+maxforsgren@users.noreply.github.com>
2020-03-04 15:00:53 +0100

    Update PaymentEvent.java

--- lab5.event/PaymentEvent.java
+++ lab5.event/PaymentEvent.java
@@ -1,24 +1,43 @@
+
 package lab5.event;
 
-import lab5.simulator.*;
-import lab5.random.*;
+import lab5.simulator.Event;
+import lab5.simulator.EventQueue;
+import lab5.state.Customer;
+import lab5.state.StoreState;
 
-public class PaymentEvent extends Event {
+public class PaymentEvent extends Event{
+		private Customer c;
+		private StoreState state;
 	
-	public PaymentEvent(double timeToStart) {
-		getTimeToStart(timeToStart);
+	public PaymentEvent(StoreState state, EventQueue ev,Customer c) {
+		this.c = c;
+		this.state = state;
+		this.eventQueue = ev;
+		this.time = state.getCheckoutTime();
+		
 	}
 	
 	public void execute() {
-		if(customerQueue.isEmpty != true) {
-			customerQueue.remove(0);
-			NOCheckouts--;
-			queue.add(New PaymentEvent);
-		}else {
-			NOCheckouts++;
+		if (state.getFreeCheckouts() == 0) {
+			state.checkoutLine.add(c);
+			state.addTotalNoQueue();
 		}
-		finishedCustomers++;
-		customersInStore--;
-		NOCheckouts++;
+		else {
+			state.addCheckoutCustomer();
+			
+		}
+		
+		
+				
 	}
+
+	@Override
+	public double getEventTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
 }
