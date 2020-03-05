6bee1ef3b915c3050b4083c54512d93165739f40
maxforsgren <61727544+maxforsgren@users.noreply.github.com>
2020-03-05 18:15:24 +0100

    Update CloseStoreEvent.java

--- lab5.event/CloseStoreEvent.java
+++ lab5.event/CloseStoreEvent.java
@@ -1,15 +1,51 @@
 package lab5.event;
 
-import lab5.simulator.*;
-import lab5.random.*;
+
+import lab5.simulator.Event;
+import lab5.simulator.EventQueue;
+import lab5.state.StoreState;
 
 public class CloseStoreEvent extends Event {
+	private double time;
+	private StoreState state;
 	
-	public CloseStoreEvent(double timeToStart) {
-		getTimeToStart();
+	/**
+	 * constructor that starts the simulation.
+	 * 
+	 * @param state the state that will be modified. 
+	 * @param queue where new events will be created and placed.
+	 */
+	public CloseStoreEvent(StoreState state, EventQueue queue) {
+		this.state = state;
+		this.eventQueue = queue;
+		this.time = state.getOpeningHours();
 	}
 	
+	/**
+	 * the changes in the event takes place here.
+	 * when the closing time for the store is reached this is where we close the store.
+	 */
 	public void execute() {
-		queue.add(New StopEvent);
+		//Update state. 
+		state.updateTime(time);
+		state.setRecentEvent("Stänger  ");
+		
+		//Stänger butiken
+					
+		
+		
+		//display view - anropa update.
+		state.updateState();
+		//state.changeDummy();
+	
+		
+		state.closeStore();
+		
+		
 	}
+
+	public double getEventTime() {
+		return this.time;
+	}
+	
 }
