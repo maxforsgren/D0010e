f81859df30679d72956bb88ec3d17027508cfff7
maxforsgren <61727544+maxforsgren@users.noreply.github.com>
2020-03-05 18:19:00 +0100

    Update StopEvent.java

--- lab5.event/StopEvent.java
+++ lab5.event/StopEvent.java
@@ -1,18 +1,48 @@
 package lab5.event;
 
-import lab5.simulator.*;
-import lab5.random.*;
+
+import lab5.simulator.Event;
+import lab5.simulator.EventQueue;
+import lab5.state.StoreState;
 
 public class StopEvent extends Event {
+	private double time;
+	private StoreState state;
 	
-	public StopEvent(double timeToStart) {
+	/**
+	 * constructor that starts the simulation.
+	 * 
+	 * @param state the state that will be modified. 
+	 * @param queue where new events will be created and placed.
+	 */
+	public StopEvent(StoreState state, EventQueue queue) {
+		this.state = state;
+		this.eventQueue = queue;
+		this.time = state.getStopTime();
+	}
+	
+	/**
+	 * the changes in the event takes place here.
+	 * when the closing time for the store is reached this is where we close the store.
+	 */
+	public void execute() {
+		//Update state. 
+		state.updateTime(this.time);
+		state.setRecentEvent("Stop");
+		state.updateState();
+		//Sätter flaggan till falsk. avslutar simuleringen.
+		state.stopSim();
+		state.updateState();
+		
+		
+		//display view - anropa update.
+		//state.updateState();
 		
 	}
-	
-	public void execute() {
-		stopSim = true;
+
+	public double getEventTime() {
+		return this.time;
 	}
 	
-
-
 }
+
