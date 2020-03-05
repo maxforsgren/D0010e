ab4e43b8f17d20becb4b1e66379543461da0521a
maxforsgren <61727544+maxforsgren@users.noreply.github.com>
2020-03-05 18:16:18 +0100

    Update PaymentEvent.java

--- lab5.event/PaymentEvent.java
+++ lab5.event/PaymentEvent.java
@@ -1,4 +1,3 @@
-
 package lab5.event;
 
 import lab5.simulator.Event;
@@ -9,33 +8,58 @@ import lab5.state.StoreState;
 public class PaymentEvent extends Event{
 		private Customer c;
 		private StoreState state;
+		private Customer c1;
 	
-	public PaymentEvent(StoreState state, EventQueue ev,Customer c) {
+	public PaymentEvent(StoreState state, EventQueue queue, Customer c) {
 		this.c = c;
 		this.state = state;
-		this.eventQueue = ev;
+		this.eventQueue = queue;
 		this.time = state.getCheckoutTime();
 		
 	}
 	
 	public void execute() {
-		if (state.getFreeCheckouts() == 0) {
-			state.checkoutLine.add(c);
-			state.addTotalNoQueue();
-		}
-		else {
-			state.addCheckoutCustomer();
-			
+		
+		state.updateTime(time);
+		state.setRecentCustomer(this.c);
+		state.setRecentEvent("Betalning");
+	
+	//detta blir som en loop först betala och lämna, sedan kollar kassakön
+	//och uppdaterar den, sedan betala och lämna osv.
+		
+		//kunden betalar sina varor och lämnar snabbköpet.
+		//1. minskar antalet kunder i snabbköpet.
+		//2. lägger till att en kund har handlat.
+		//3. En kassa blir ledig.
+		state.removeCustomerInStore();		//minska antalet kunder i snabbköpet.		
+		state.addFinishedCustomers();
+		//lägger till kunder som har betalat/handlat.
+		//tar bort en person från kassan "en kassa blir ledig".
+		state.removeCheckoutCustomer();	
+		//4. Kollar om kassakön inte är tom
+		//5. Kund som stått längst i kön/först i arrayen.
+		//6. Går till den lediga kassan.
+		//7. Genererar en betalningshändelse för C.
+		if(!state.isCheckoutLineEmpty()) {		
+			System.out.println("Kön är inte tom");//Om kassakön ej är tom.
+			c1 = state.getFirstCheckoutLine();		//Den som är först i kön går till kassa/betalar.
+			state.removeFromCheckoutLine();			//Minskar kön från första position.
+			state.addCheckoutCustomer();			//En kassa blir upptagen.    	
+			eventQueue.add(new PaymentEvent(state, eventQueue, c1));
 		}
 		
+		
+		
+		//Display view
+		state.updateCheckQueue(eventQueue);
+		state.updateState();
 		
 				
 	}
 
-	@Override
+	
 	public double getEventTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.time;
 	}
 	
 	
